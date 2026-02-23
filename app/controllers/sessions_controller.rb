0a758dcde9af759eab51eeed9454ade831eddbab
Joel Hunt <itsjoelhunt@gmail.com>
2026-02-23 14:34:37 -0600

    feat: Reset user session on login and apply yellow styling to pending blocking compliance reviews in the index view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
+      reset_session
       session[:user_id] = user.id
       redirect_to root_path, notice: "Logged in successfully."
     else
